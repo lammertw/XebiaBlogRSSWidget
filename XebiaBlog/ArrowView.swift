d6a152da2f163449d6e97da713e2c3fd134c3164
Lammert Westerhoff <westerhoff@gmail.com>
2014-09-09 10:19:23 +0200

    Initial commit

---
+++ XebiaBlog/ArrowView.swift
@@ -0,0 +1,18 @@
+//
+//  ArrowView.swift
+//  XebiaBlog
+//
+//  Created by Lammert Westerhoff on 09/09/14.
+//  Copyright (c) 2014 Lammert Westerhoff. All rights reserved.
+//
+
+import UIKit
+
+class ArrowView: UIView {
+
+    override func drawRect(rect: CGRect)
+    {
+        XebiaBlogStyleKit.drawArrow(rect, arrowStroke: tintColor)
+    }
+
+}
