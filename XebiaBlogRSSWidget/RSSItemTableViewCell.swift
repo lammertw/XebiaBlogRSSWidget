4d218bcf2be456358bb73328586630aa774042fa
Lammert Westerhoff <westerhoff@gmail.com>
2014-09-09 13:20:53 +0200

    Removed unused code

--- XebiaBlogRSSWidget/RSSItemTableViewCell.swift
+++ XebiaBlogRSSWidget/RSSItemTableViewCell.swift
@@ -14,15 +14,4 @@ class RSSItemTableViewCell: UITableViewCell {
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
